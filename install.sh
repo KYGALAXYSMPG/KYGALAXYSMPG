345ff8bef257054678798031b6529be26cb486da
KYGALAXYSMPG <galaxysmpg@gmail.com>
2024-11-12 09:46:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ -f "proxy" ]; then
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/KYGALAXYSMPG/KYGALAXYSMPG/blob/main/PROXYLEON
+wget -q https://github.com/KYGALAXYSMPG/KYGALAXYSMPG/blob/main/proxyj
 sleep 1
 echo -e "${GREEN}LeonProxy is now Installed.${ENDCOLOR}"
 echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
